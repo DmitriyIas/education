d8dacd5c03ef232c71d3e301e893583ffd41567f
dmitriy <dmitriy.jasinskiy@gmail.com>
2018-07-24 20:39:50 +0300

    Sprint 1

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -7,27 +7,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        getRun();
+        Runner runner = new Runner();
+        runner.getRun();
     }
 
-    private static void getRun() {
-        PalindromeVerification palindrome = new PalindromeVerification();
-        MinMaxAvgNumber number = new MinMaxAvgNumber();
-        StringReverse line = new StringReverse();
-        System.out.println("Enter exercise you want to run: \n palindrome \n number \n reverse");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
 
-        if ("palindrome".equals(input)) {
-            palindrome.verification();
-
-        } else if ("number".equals(input)) {
-            number.minMaxAvgCalculation();
-
-        } else if ("reverse".equals(input)) {
-            line.useReverse();
-
-        }
-
-    }
 }
