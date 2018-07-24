d188121ee5d997b9962a95e8291bc684b72c323f
dmitriy <dmitriy.jasinskiy@gmail.com>
2018-07-24 21:28:34 +0300

    main update

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,15 +1,9 @@
-import MinMaxAvgNumber.MinMaxAvgNumber;
-import PalindromeVerification.PalindromeVerification;
-import StringReverse.StringReverse;
 
 public class Main {
 
     public static void main(String[] args) {
-
         Runner runner = new Runner();
         runner.getRun();
     }
-
-
 
 }
