d603c20b03f4e11e9fa641a2411addde68a133cd
dmitriy <dmitriy.jasinskiy@gmail.com>
2018-07-24 20:44:39 +0300

    Sprint 1

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,14 +2,14 @@ import MinMaxAvgNumber.MinMaxAvgNumber;
 import PalindromeVerification.PalindromeVerification;
 import StringReverse.StringReverse;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
+
         Runner runner = new Runner();
         runner.getRun();
     }
+
 
 
 }
