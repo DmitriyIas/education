85382638248e2ab16d221ceceb2aa09711c2f3c3
dmitriy <dmitriy.jasinskiy@gmail.com>
2018-07-24 20:31:35 +0300

    First Sprint

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,31 +2,32 @@ import MinMaxAvgNumber.MinMaxAvgNumber;
 import PalindromeVerification.PalindromeVerification;
 import StringReverse.StringReverse;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        getRun();
+    }
 
+    private static void getRun() {
         PalindromeVerification palindrome = new PalindromeVerification();
         MinMaxAvgNumber number = new MinMaxAvgNumber();
         StringReverse line = new StringReverse();
+        System.out.println("Enter exercise you want to run: \n palindrome \n number \n reverse");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
 
-        switch (Run.number){
-            case palindrome:
-                palindrome.verification();
-                break;
-            case number:
-                number.minMaxAvgCalculation();
-                break;
-            case reverse:
-                line.useReverse();
-                break;
+        if ("palindrome".equals(input)) {
+            palindrome.verification();
+
+        } else if ("number".equals(input)) {
+            number.minMaxAvgCalculation();
+
+        } else if ("reverse".equals(input)) {
+            line.useReverse();
+
         }
-    }
 
-    public enum Run{
-        palindrome,
-        number,
-        reverse
     }
-
 }
