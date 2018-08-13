1da768365a16a3770387747f62db2ba0c69fd4e9
dmitriy <dmitriy.jasinskiy@gmail.com>
2018-08-13 13:53:33 +0300

    Palindrome - cycle fixed Employee - convertation of data types received by the object in the form of parameters - fixed Employee - overrided toString method Employee - union for and while - fixed

--- src/main/java/employee/EmployeeUtils.java
+++ src/main/java/employee/EmployeeUtils.java
@@ -3,7 +3,6 @@ package employee;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
