dc8411755bce767cde9b33270fc0dbbc8bf07ec6
dmitriy <dmitriy.jasinskiy@gmail.com>
2018-09-27 16:23:26 +0300

    - fix runner

--- src/test/java/palindromeVerificationTest/PalindromeVerificationTest.java
+++ src/test/java/palindromeVerificationTest/PalindromeVerificationTest.java
@@ -3,13 +3,19 @@ package palindromeVerificationTest;
 import org.junit.Test;
 import palindromeVerification.PalindromeVerification;
 
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 public class PalindromeVerificationTest {
 
     @Test
-    public void testPalindrome(){
+    public void testPositive(){
         assertTrue(PalindromeVerification.isPalindrome("ababa"));
+    }
+
+    @Test
+    public void testNegative(){
+        assertFalse(PalindromeVerification.isPalindrome("ababahdhg"));
     }
 
 }
