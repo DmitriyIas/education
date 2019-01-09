0b45e9e289a9016c86d5566c6115a2ae546bc394
dmytro.iasynskyi <dmytro.iasynskyi@vitech.com.ua>
2019-01-09 18:38:56 +0200

    LinkedList code refactor

--- src/main/java/linkedList/MyLinkedList.java
+++ src/main/java/linkedList/MyLinkedList.java
@@ -33,28 +33,5 @@ public class MyLinkedList<T> {
         return result.toString();
     }
 
-    private class Node {
-        private T value;
-        private Node next;
 
-        public Node(T value){
-            this.value = value;
-        }
-
-        public T getValue() {
-            return value;
-        }
-
-        public void setValue(T value) {
-            this.value = value;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
-    }
 }
